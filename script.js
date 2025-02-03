cada3678d5e70d7fe0bed9adcbbe5e5f7613d4c8
RulerPrime <irletrondan@gmail.com>
2025-02-03 12:14:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 // 1. Import the all three objects (Song, Musician, Playlist) that you created in the MusicData.js module file
-
+import { Song, Muscician, Playlist } from "./MusicData.js";
 
 // 2. declare a new constant and assign to it a new musician object to describe the musician for the first song.
 
-
-
 // 3. declare a new constant and assign to it a new musician object to describe the musician for the second song.
 
-
-
-
 // 4. declare a new constant and assign to it a new musician object to describe the musician for the third song.
-
 
 // 5. declare a new constant and assign to it a new Song object to describe the first song
 // (make sure to use the Musician object from step 2 in creating this Song object
 
-
-
 // 6. declare a new constant and assign to it a new Song object to describe the second song
 // (make sure to use the Musician object from step 3 in creating this Song object
 
-
 // 7. declare a new constant and assign to it a new Song object to describe the third song
 // (make sure to use the Musician object from step 4 in creating this Song object
-
 
 // 8. declare a new constant called myPlaylist and assign to it a new Playlist object and make sure to use the three Song objects
 //from steps 5, 6, & 7 when creating it.
 //It must be called myPlaylist
 //It must be called myPlaylist
 
-
 //9. call the .getInfo() method on myPlaylist
 
+const myPlaylist = new Playlist();
+
+myPlaylist.getInfo();
 
 //DO NOT EDIT BELOW THIS LINE
 //the code below here will insert the Playlist data into the webpage.
